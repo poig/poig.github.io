24b8b11553da9968116743c4b26aa24197225e79
poig <laolianglaoliang@gmail.com>
2023-06-14 07:13:04 +0800

    fix article

--- java/oop_gui/demo/oop_example/src/user_function/Article.java
+++ java/oop_gui/demo/oop_example/src/user_function/Article.java
@@ -4,7 +4,9 @@ import main.main;
 import javax.swing.*;
 import java.awt.*;
 import java.awt.event.*;
+import java.io.BufferedReader;
 import java.io.File;
+import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
 import java.io.PrintWriter;
@@ -42,10 +44,12 @@ public class Article {
         + "\n"
         + "Take the production of shirts and shoes, which has more than doubled in the past quarter century — three quarters end up burned or buried in landfills. This feels like a personal failure of sorts. For many years, I was the COO of Timberland, a footwear and apparel brand that aspired to lead the industry toward a more sustainable future. The reasons for the industry’s sustainability letdown are complicated. Pressure for unrelenting growth summed with consumer demand for cheap, fast fashion have been a major contributors.  So too are the related facts that real prices for footwear and apparel have halved since 1990 with most new items made from non-biodegradable petroleum-based synthetics."};
 
-    public void saveText(String f, String s) throws IOException {
+    public void saveText(String f, String s) {
 
-        try (FileWriter writer = new FileWriter(f, true);) {
+        try (FileWriter writer = new FileWriter(f);) {
             writer.write(s);
+        } catch (IOException e) {
+
         }
         System.out.println("saved");
 
@@ -58,31 +62,32 @@ public class Article {
         if (!new File(files[0]).exists()) {
             File article_folder = new File(files[0]);
             article_folder.getParentFile().mkdirs();
-            main.inp1 = addcontent[0];
-            main.inp2 = addcontent[1];
-            main.inp3 = addcontent[2];
+            for (int i = 0; i < 3; i++) {
+                saveText(files[i], addcontent[i]);
+            }
+            //main.inp1 = addcontent[0];
+            // main.inp2 = addcontent[1];
+            //main.inp3 = addcontent[2];
         }
 
-        StringBuffer buffer1 = new StringBuffer();
-        RandomAccessFile r1 = new RandomAccessFile(new File(files[0]), "rw");
-        while (r1.getFilePointer() < r1.length()) {
-            buffer1.append(r1.readLine() + System.lineSeparator());
-        }
-        content[0] = buffer1.toString();
+        for (int i = 0; i < 3; i++) {
+            StringBuilder sb = new StringBuilder();
 
-        StringBuffer buffer2 = new StringBuffer();
-        RandomAccessFile r2 = new RandomAccessFile(new File(files[1]), "rw");
-        while (r2.getFilePointer() < r2.length()) {
-            buffer2.append(r2.readLine() + System.lineSeparator());
-        }
-        content[1] = buffer2.toString();
+            try (BufferedReader r1 = new BufferedReader(new FileReader(files[i]));) {
+                //}
+                //try (BufferedReader reader = new BufferedReader(new FileReader("dictionary.txt"))) {
+                String line = r1.readLine();
 
-        StringBuffer buffer3 = new StringBuffer();
-        RandomAccessFile r3 = new RandomAccessFile(new File(files[2]), "rw");
-        while (r3.getFilePointer() < r3.length()) {
-            buffer3.append(r3.readLine() + System.lineSeparator());
+                while (line != null) {
+                    sb.append(line);
+                    sb.append(System.lineSeparator());
+                    line = r1.readLine();
+                }
+                content[i] = sb.toString();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
         }
-        content[2] = buffer3.toString();
 
         f = new JFrame();
         f.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
@@ -103,7 +108,7 @@ public class Article {
         list1.setFont(font);
         list1.setLineWrap(true);
         list1.setWrapStyleWord(true);
-        list1.setText(main.inp1);
+        list1.setText(content[0]);
         list1.setEditable(false);
         JScrollPane sp1 = new JScrollPane(list1, JScrollPane.VERTICAL_SCROLLBAR_ALWAYS,
                 JScrollPane.HORIZONTAL_SCROLLBAR_ALWAYS);
@@ -112,7 +117,7 @@ public class Article {
         list2.setFont(font);
         list2.setLineWrap(true);
         list2.setWrapStyleWord(true);
-        list2.setText(main.inp2);
+        list2.setText(content[1]);
         list2.setEditable(false);
         JScrollPane sp2 = new JScrollPane(list2, JScrollPane.VERTICAL_SCROLLBAR_ALWAYS,
                 JScrollPane.HORIZONTAL_SCROLLBAR_ALWAYS);
@@ -121,7 +126,7 @@ public class Article {
         list3.setFont(font);
         list3.setLineWrap(true);
         list3.setWrapStyleWord(true);
-        list3.setText(main.inp3);
+        list3.setText(content[2]);
         list3.setEditable(false);
         JScrollPane sp3 = new JScrollPane(list3, JScrollPane.VERTICAL_SCROLLBAR_ALWAYS,
                 JScrollPane.HORIZONTAL_SCROLLBAR_ALWAYS);
@@ -144,23 +149,18 @@ public class Article {
         back.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
-                if (!list1.getText().equals(main.inp1) || !list2.getText().equals(main.inp2) || !list3.getText().equals(main.inp3)) {
-                    main.inp1 = list1.getText();
-                    main.inp2 = list2.getText();
-                    main.inp3 = list3.getText();
-                    try {
-                        saveText(files[0], main.inp1);
-                        saveText(files[1], main.inp2);
-                        saveText(files[2], main.inp3);
-                    } catch (Exception ex) {
-                        ex.printStackTrace();
-                        System.out.print(ex);
-                    }
-                } else {
-                    main.inp1 = main.inp1;
-                    main.inp2 = main.inp2;
-                    main.inp3 = main.inp3;
-                }
+                /*
+                if (!list1.getText().equals(content[0]) || !list2.getText().equals(content[1]) || !list3.getText().equals(content[2])) {
+                    //main.inp1 = list1.getText();
+                    //main.inp2 = list2.getText();
+                    //main.inp3 = list3.getText();
+                    saveText(files[0], list1.getText());
+                    saveText(files[1], list2.getText());
+                    saveText(files[2], list3.getText());
+                }*/
+                saveText(files[0], list1.getText());
+                saveText(files[1], list2.getText());
+                saveText(files[2], list3.getText());
                 //f.setVisible(false);
                 f.dispose();
             }
